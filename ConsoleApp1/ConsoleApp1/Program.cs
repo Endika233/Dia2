25a6a94447edd515f645dd36e680b671a10bc892
Endika233 <43742370+Endika233@users.noreply.github.com>
2018-10-02 09:36:58 +0200

    .

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce dos números");
+            int num1 = Int32.Parse(Console.ReadLine()), num2 = Int32.Parse(Console.ReadLine());
+            if (num1 == num2) 
+            { Console.WriteLine("Los números son iguales."); }
+            else
+            { Console.WriteLine("Los números no son iguales"); }
+
+            Console.ReadKey();
+        }
+    }
+}
