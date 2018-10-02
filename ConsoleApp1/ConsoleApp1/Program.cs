b97bba3b5224e0da198832aded2fd3afe0cfde5b
Endika233 <43742370+Endika233@users.noreply.github.com>
2018-10-02 09:43:36 +0200

    .

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,12 +10,21 @@ namespace Condicionales
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Introduce dos números");
+            /*Console.WriteLine("Introduce dos números");
             int num1 = Int32.Parse(Console.ReadLine()), num2 = Int32.Parse(Console.ReadLine());
             if (num1 == num2) 
             { Console.WriteLine("Los números son iguales."); }
             else
-            { Console.WriteLine("Los números no son iguales"); }
+            { Console.WriteLine("Los números no son iguales"); }*/
+
+
+            int num1;//LAS VARIABLES AL PRINCIPIO PARA TENERLAS LOCALIZADAS
+            Console.WriteLine("Introduce un número");
+            num1 = Int32.Parse(Console.ReadLine());
+            if (num1 % 2 == 0)
+            { Console.WriteLine("El número es par."); }
+            else
+            { Console.WriteLine("El número es impar"); }
 
             Console.ReadKey();
         }
